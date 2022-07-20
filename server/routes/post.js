cb7c49f684353256db5543460ed5c84dd548a8ac
hyebin829 <smilerain779@gmail.com>
2022-07-21 00:41:50 +0900

    Feat : get popular snack router

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const router = express.Router()
-const { Snack, Review, User, Comment } = require('../models')
+const { Snack, Review, User, Comment, sequelize } = require('../models')
+
+// "SELECT COUNT(*), SnackId
+// FROM Favorite GROUP BY SnackId ORDER BY COUNT(*) DESC"
 
 router.get('/popularsnack', async (req, res) => {
-    const popularsnacklist = await snack.getFavorites({
-        where : {
-            
-        }
+  const query = `
+    SELECT COUNT(*) as count, SnackId
+    FROM Favorite
+    GROUP BY SnackId
+    ORDER BY count DESC`
+  try {
+    const popularsnacklist = await sequelize.query(query, {
+      type: sequelize.QueryTypes.SELECT,
     })
+    res.status(200).json(popularsnacklist)
+  } catch (error) {
+    console.error(error)
+  }
 })
+
+module.exports = router
