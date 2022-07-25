014fcf463baf66ad9bc5bc4066c1317a0ac724d2
hyebin829 <smilerain779@gmail.com>
2022-07-25 23:38:21 +0900

    Feat : passport kakao

---
+++ server/passport/index.js
@@ -0,0 +1,32 @@
+const passport = require('passport')
+const kakao = require('./kakao')
+const { User } = require('../models')
+
+module.exports = () => {
+  //router에서 req.login 했을 경우
+  passport.serializeUser((user, done) => {
+    done(null, {
+      id: user.id,
+      accessToken: user.accessToken ? user.accessToken : '',
+      refreshToken: user.refreshToken ? user.refreshToken : '',
+    })
+  })
+  passport.deserializeUser(async (data, done) => {
+    try {
+      const user = await User.findOne({
+        where: { id: data.id },
+      })
+      const userWithToken = {
+        id: user.id,
+        accessToken: data.accessToken ? data.accessToken : '',
+        refreshToken: data.refreshToken ? data.refreshToken : '',
+      }
+      done(null, userWithToken)
+    } catch (error) {
+      console.error(error)
+      done(error)
+    }
+  })
+
+  kakao()
+}
---
+++ server/passport/kakao.js
@@ -0,0 +1,46 @@
+const passport = require('passport')
+const KakaoStrategy = require('passport-kakao').Strategy
+const { User } = require('../models')
+
+module.exports = () => {
+  passport.use(
+    new KakaoStrategy(
+      {
+        clientID: process.env.KAKAO_KEY,
+        callbackURL: '/user/kakao/login/callback',
+      },
+
+      async (accessToken, refreshToken, profile, done) => {
+        console.log('kakao profile', profile, accessToken, refreshToken)
+        try {
+          const exUser = await User.findOne({
+            where: { email: profile.id },
+          })
+          if (exUser) {
+            const userWithToken = {
+              id: exUser.id,
+              accessToken: accessToken,
+              refreshToken: refreshToken,
+            }
+            done(null, userWithToken)
+          } else {
+            const newUser = await User.create({
+              email: profile.id,
+              nickname: `유저${profile.id}`,
+              password: 'kakaologin',
+            })
+            const newUserwithToken = {
+              id: newUser.id,
+              accessToken: accessToken,
+              refreshToken: refreshToken,
+            }
+            done(null, newUserwithToken)
+          }
+        } catch (error) {
+          console.error(error)
+          done(error)
+        }
+      }
+    )
+  )
+}
