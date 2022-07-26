434726114246646a27ba4b2b4c2157905d5d66c1
hyebin829 <smilerain779@gmail.com>
2022-07-26 20:32:22 +0900

    Feat : add router - get popular snack, top rating snack, top review snack router

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,34 +1,76 @@
 const express = require('express')
 const router = express.Router()
-const { Snack, Review, User, Comment, sequelize } = require('../models')
+const { sequelize } = require('../models')
+const { Op } = require('sequelize')
 
 router.get('/popularsnack', async (req, res, next) => {
   const query = `
-    SELECT COUNT(*) as count, SnackId
-    FROM Favorite
-    GROUP BY SnackId
-    ORDER BY count DESC
-    LIMIT 10`
+    WITH CNT as (
+      SELECT COUNT(*) as count, SnackId
+      FROM Favorite
+      GROUP BY SnackId
+      )
+      SELECT id,name,brand,imagesrc,country,count FROM snacks
+      LEFT JOIN CNT
+      ON CNT.SnackId = snacks.id
+      WHERE SnackId > 0
+      ORDER BY count DESC
+      LIMIT 10
+      `
   try {
-    const selectedSnack = await sequelize.query(query, {
+    const popularSnackList = await sequelize.query(query, {
       type: sequelize.QueryTypes.SELECT,
     })
-    const idList = selectedSnack.map((x) => x.SnackId)
-    const countList = selectedSnack.map((x) => x.count)
-
-    const popularSnackList = await Snack.findAll({
-      where: {
-        id: idList.map((x) => x),
-      },
-      attributes: ['id', 'name', 'brand', 'imagesrc', 'country'],
-      raw: true,
-    })
-
-    for (let i = 0; i < popularSnackList.length; i++) {
-      popularSnackList[i].count = countList[i]
-    }
-
     res.status(201).json(popularSnackList)
+  } catch (error) {
+    console.error(error)
+    next(error)
+  }
+})
+
+router.get('/topratingsnack', async (req, res, next) => {
+  const query = `
+  WITH CNT as (
+    SELECT AVG(rating) as rating , SnackId FROM reviews
+    GROUP BY SnackId
+    )
+    SELECT id,name,brand,imagesrc,country,rating FROM snacks
+    LEFT JOIN CNT
+    ON CNT.SnackId = snacks.id
+    WHERE rating >= 4
+    ORDER BY rating DESC
+    LIMIT 10
+    `
+
+  try {
+    const topRatingSnackList = await sequelize.query(query, {
+      type: sequelize.QueryTypes.SELECT,
+    })
+    res.status(201).json(topRatingSnackList)
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+router.get('/topreview', async (req, res, next) => {
+  const query = `
+  WITH CNT as (
+    SELECT COUNT(*) as count, SnackId
+    FROM reviews
+    GROUP BY SnackId
+    )
+    SELECT id,name,brand,imagesrc,country,count FROM snacks
+    LEFT JOIN CNT
+    ON CNT.SnackId = snacks.id
+    WHERE SnackId > 0
+    ORDER BY count DESC
+    LIMIT 10`
+
+  try {
+    const topreviewList = await sequelize.query(query, {
+      type: sequelize.QueryTypes.SELECT,
+    })
+    res.status(201).json(topreviewList)
   } catch (error) {
     console.error(error)
     next(error)
