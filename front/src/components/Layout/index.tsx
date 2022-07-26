49639c025a0d13c519e40e1eed9122e88bff8e5f
hyebin829 <smilerain779@gmail.com>
2022-07-26 22:48:57 +0900

    Feat : add login modal , modal option

--- front/src/components/Layout/index.tsx
+++ front/src/components/Layout/index.tsx
@@ -1,13 +1,34 @@
 import GNB from 'components/GNB'
 import Header from 'components/Header'
+import LoginModal from 'components/LoginModal'
+import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import styles from './layout.module.scss'
 
 const Layout = () => {
+  const [isModalView, setIsModalView] = useState(false)
+
+  useEffect(() => {
+    const $body = document.querySelector('body')
+    if (isModalView) {
+      $body!.style.overflow = 'hidden'
+    }
+
+    return () => {
+      $body!.style.overflow = 'auto'
+    }
+  }, [isModalView])
+
+  const toggleModal = () => {
+    setIsModalView((prev) => !prev)
+  }
+
   return (
-    <div>
-      <Header />
+    <div className={styles.layout}>
+      <Header toggleModal={toggleModal} />
       <GNB />
       <main>
+        {isModalView && <LoginModal toggleModal={toggleModal} />}
         <Outlet />
       </main>
       <footer />
