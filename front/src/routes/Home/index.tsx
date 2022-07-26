44f771fd6da2933db31f9e57cbaf973a79121055
hyebin829 <smilerain779@gmail.com>
2022-07-26 22:49:51 +0900

    Feat : load top review snack list

--- front/src/routes/Home/index.tsx
+++ front/src/routes/Home/index.tsx
@@ -1,21 +1,20 @@
 import { useAppDispatch, useAppSelector } from 'hooks/useRedux'
 import { Fragment, useEffect } from 'react'
 import styles from './home.module.scss'
-import { loadPopularSnack, loadTopRatingSnack } from 'actions/post'
+import { loadPopularSnack, loadTopRatingSnack, loadTopReviewSnack } from 'actions/post'
 import { loadMyInfo } from 'actions/user'
 
 const Home = () => {
   const dispatch = useAppDispatch()
-  const { popularSnackList, topRatingSnackList } = useAppSelector((state) => state.post)
+  const { popularSnackList, topRatingSnackList, topReviewSnackList } = useAppSelector((state) => state.post)
   const { myInfo } = useAppSelector((state) => state.user)
 
   useEffect(() => {
     dispatch(loadPopularSnack())
     dispatch(loadTopRatingSnack())
+    dispatch(loadTopReviewSnack())
     dispatch(loadMyInfo())
   }, [dispatch])
-
-  console.log(myInfo)
 
   return (
     <div className={styles.homeWrapper}>
@@ -34,14 +33,24 @@ const Home = () => {
       <ul>
         {topRatingSnackList.map((item) => (
           <Fragment key={item.id}>
+            <li>{item.rating.toFixed(1)}</li>
             <li>{item.name}</li>
             <li>{item.brand}</li>
-            <li>{item.rating.toFixed(1)}</li>
             <img src={`http://localhost:3065/snackimage/${item.imagesrc}`} alt={item.name} />
           </Fragment>
         ))}
       </ul>
       <div>리뷰가 많은 과자</div>
+      <ul>
+        {topReviewSnackList.map((item) => (
+          <Fragment key={item.id}>
+            <li>{item.name}</li>
+            <li>{item.brand}</li>
+            <li>{item.count}</li>
+            <img src={`http://localhost:3065/snackimage/${item.imagesrc}`} alt={item.name} />
+          </Fragment>
+        ))}
+      </ul>
     </div>
   )
 }
