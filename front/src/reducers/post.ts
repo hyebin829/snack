24b5bcfdc5672edd57af1531b8fde3c949f1d258
hyebin829 <smilerain779@gmail.com>
2022-07-28 00:34:18 +0900

    Feat : add reducer -  - load top review snack, load search word, load snack info action

--- front/src/reducers/post.ts
+++ front/src/reducers/post.ts
@@ -1,16 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { loadPopularSnack, loadTopRatingSnack } from 'actions/post'
+import { loadPopularSnack, loadSearchWord, loadSnackInfo, loadTopRatingSnack, loadTopReviewSnack } from 'actions/post'
 import { IpostState } from 'types/post'
 
 export const initialState: IpostState = {
   popularSnackList: [],
   topRatingSnackList: [],
+  topReviewSnackList: [],
+  searchWordList: [],
+  snackInfo: [],
   loadPopularSnackLoading: false,
   loadPopularSnackDone: false,
   loadPopularSnackError: null,
   loadTopRatingSnackLoading: false,
   loadTopRatingSnackDone: false,
   loadTopRatingSnackError: null,
+  loadTopReviewSnackLoading: false,
+  loadTopReviewSnackDone: false,
+  loadTopReviewSnackError: null,
+  loadSearchWordLoading: false,
+  loadSearchWordDone: false,
+  loadSearchWordError: null,
+  loadSnackInfoLoading: false,
+  loadSnackInfoDone: false,
+  loadSnackInfoError: null,
 }
 
 const postSlice = createSlice({
@@ -46,6 +58,48 @@ const postSlice = createSlice({
       .addCase(loadTopRatingSnack.rejected, (state, action) => {
         state.loadTopRatingSnackLoading = false
         state.loadTopRatingSnackError = action.error.message
+      })
+      .addCase(loadTopReviewSnack.pending, (state) => {
+        state.loadTopReviewSnackLoading = true
+        state.loadTopReviewSnackDone = false
+        state.loadTopReviewSnackError = null
+      })
+      .addCase(loadTopReviewSnack.fulfilled, (state, action) => {
+        state.loadTopReviewSnackLoading = false
+        state.loadTopReviewSnackDone = true
+        state.topReviewSnackList = action.payload
+      })
+      .addCase(loadTopReviewSnack.rejected, (state, action) => {
+        state.loadTopReviewSnackLoading = false
+        state.loadTopReviewSnackError = action.error.message
+      })
+      .addCase(loadSearchWord.pending, (state) => {
+        state.loadSearchWordLoading = true
+        state.loadSearchWordDone = false
+        state.loadSearchWordError = null
+      })
+      .addCase(loadSearchWord.fulfilled, (state, action) => {
+        state.loadSearchWordLoading = false
+        state.loadSearchWordDone = true
+        state.searchWordList = action.payload
+      })
+      .addCase(loadSearchWord.rejected, (state, action) => {
+        state.loadSearchWordLoading = false
+        state.loadSearchWordError = action.error.message
+      })
+      .addCase(loadSnackInfo.pending, (state) => {
+        state.loadSnackInfoLoading = true
+        state.loadSnackInfoDone = false
+        state.loadSnackInfoError = null
+      })
+      .addCase(loadSnackInfo.fulfilled, (state, action) => {
+        state.loadSnackInfoLoading = false
+        state.loadSnackInfoDone = true
+        state.snackInfo = action.payload
+      })
+      .addCase(loadSnackInfo.rejected, (state, action) => {
+        state.loadSnackInfoLoading = false
+        state.loadSnackInfoError = action.error.message
       })
       .addDefaultCase((state) => state),
 })
