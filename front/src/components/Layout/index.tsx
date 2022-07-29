abaf1e3043130ee438edc36d608c67903e812e33
hyebin829 <smilerain779@gmail.com>
2022-07-29 20:29:37 +0900

    Feat : add loadmyinfo action

--- front/src/components/Layout/index.tsx
+++ front/src/components/Layout/index.tsx
@@ -1,12 +1,19 @@
+import { loadMyInfo } from 'actions/user'
 import GNB from 'components/GNB'
 import Header from 'components/Header'
 import LoginModal from 'components/LoginModal'
+import { useAppDispatch } from 'hooks/useRedux'
 import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import styles from './layout.module.scss'
 
 const Layout = () => {
   const [isModalView, setIsModalView] = useState(false)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(loadMyInfo())
+  }, [dispatch])
 
   useEffect(() => {
     const $body = document.querySelector('body')
