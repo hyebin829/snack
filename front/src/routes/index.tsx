2f0a0bf19e61edae6a2c96099dc11bd007c6d3cf
hyebin829 <smilerain779@gmail.com>
2022-08-05 21:32:52 +0900

    Feat : review, reviewlist router

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -2,6 +2,8 @@ import Layout from 'components/Layout'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import ProfilePage from './ProfilePage'
+import ReviewListPage from './ReviewListPage'
+import ReviewWritePage from './ReviewWritePage'
 import SearchPage from './SearchPage'
 import SnackPage from './SnackPage'
 
@@ -14,6 +16,8 @@ const App = () => {
           <Route path='/profile' element={<ProfilePage />} />
           <Route path='/search' element={<SearchPage />} />
           <Route path='/snack/:id' element={<SnackPage />} />
+          <Route path='/snack/:id/review' element={<ReviewWritePage />} />
+          <Route path='/snack/:id/reviewlist' element={<ReviewListPage />} />
         </Route>
       </Routes>
     </div>
