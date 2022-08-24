15f66d34eb796ff636b2e579251dce33ed9eb7d3
hyebin829 <smilerain779@gmail.com>
2022-08-24 11:11:50 +0900

    Style : add login modal style

--- front/src/components/LoginModal/index.tsx
+++ front/src/components/LoginModal/index.tsx
@@ -1,5 +1,6 @@
 import Portal from 'Portal'
 import styles from './loginmodal.module.scss'
+import { IoMdClose } from 'react-icons/io'
 
 type ToggleModal = {
   toggleModal: React.MouseEventHandler<HTMLButtonElement>
@@ -10,9 +11,13 @@ const LoginModal = ({ toggleModal }: ToggleModal) => {
     <Portal>
       <div className={styles.modalBackground}>
         <div className={styles.modalWrapper}>
-          <div>로그인</div>
+          <h2>SNACK</h2>
+          <div className={styles.loginTitle}>
+            카카오 로그인으로 <br />
+            간편하게 이용이 가능합니다.
+          </div>
           <button type='button' onClick={toggleModal}>
-            닫기
+            <IoMdClose size={20} />
           </button>
           <a href='http://localhost:3065/user/kakao/login'>
             <img src='/images/kakao_login_medium_narrow.png' alt='kakao login' />
