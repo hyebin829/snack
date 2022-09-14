0fb9cfcbdd66ce6c90e659fd7eba3d16edf7b21d
hyebin829 <smilerain779@gmail.com>
2022-09-14 20:57:59 +0900

    Feat : review like

---
+++ front/src/components/LikeButton/index.tsx
@@ -0,0 +1,29 @@
+import { addLike, removeLike } from 'actions/post'
+import { useAppDispatch, useAppSelector } from 'hooks/useRedux'
+import { RiThumbUpLine, RiThumbUpFill } from 'react-icons/ri'
+import { IbestReview, Ireview } from 'types/post'
+
+type review = {
+  review: Ireview | IbestReview
+}
+
+const LikeButton = ({ review }: review) => {
+  const dispatch = useAppDispatch()
+  const { myInfo } = useAppSelector((state) => state.user)
+
+  const toggleReviewLike = () => {
+    liked
+      ? dispatch(removeLike({ reviewId: review.id }))
+      : dispatch(addLike({ reviewId: review.id }))
+  }
+
+  const liked = review.Likers.find((liker) => liker.id === myInfo?.id)
+
+  return (
+    <button type='button' onClick={toggleReviewLike}>
+      {liked ? <RiThumbUpFill /> : <RiThumbUpLine />}
+    </button>
+  )
+}
+
+export default LikeButton
