551be3377a1e540d578fe9bf1c5b31bf94ff9ef8
hyebin829 <smilerain779@gmail.com>
2022-09-20 18:34:00 +0900

    Style : add style

--- front/src/routes/SnackPage/index.tsx
+++ front/src/routes/SnackPage/index.tsx
@@ -1,13 +1,14 @@
 import { loadSnackInfo } from 'actions/post'
 import FavoriteButton from 'components/FavoriteButton'
 import { useAppDispatch, useAppSelector } from 'hooks/useRedux'
-import { Fragment, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import ReviewListPage from 'routes/ReviewListPage'
 import styles from './snack.module.scss'
 import { AiFillStar } from 'react-icons/ai'
 import { openConfirmModal, openLoginModal, openReviewModal } from 'reducers/modal'
 import BestReview from 'components/BestReview'
+import { HiPencilAlt } from 'react-icons/hi'
 
 const SnackPage = () => {
   const params = useParams()
@@ -28,10 +29,6 @@ const SnackPage = () => {
     )
   }
 
-  const handleOpenModal = (reviewId: number) => {
-    dispatch(openConfirmModal(reviewId))
-  }
-
   const handleOpenReviewModal = () => {
     if (!myInfo?.id) {
       dispatch(openLoginModal())
@@ -42,9 +39,9 @@ const SnackPage = () => {
   const myReview = snackInfo?.Reviews.filter((review) => review.UserId === myInfo?.id)
 
   return (
-    <div>
-      <FavoriteButton snackId={snackId} />
+    <div className={styles.snackPage}>
       <ul className={styles.snackInfoWrapper}>
+        <FavoriteButton snackId={snackId} />
         <img
           src={`http://localhost:3065/snackimage/${snackInfo?.imagesrc}`}
           alt={snackInfo?.name}
@@ -52,30 +49,25 @@ const SnackPage = () => {
         <li className={styles.snackName}>{snackInfo?.name}</li>
         <li className={styles.snackBrand}>{snackInfo?.brand}</li>
         <li className={styles.rating}>
-          평균 <AiFillStar size={14} /> {isNaN(ratingAverage) ? 0 : ratingAverage}점
+          평균 <AiFillStar size={15} color='#ffa500' /> {isNaN(ratingAverage) ? 0 : ratingAverage}점
+          <span>({snackInfo?.Reviews.length.toLocaleString()}명)</span>
         </li>
       </ul>
-      {myReview?.length ? (
-        <div>
-          <ul>
-            {myReview.map((review) => (
-              <Fragment key={`myreview-${review.id}`}>
-                <li>{review.content}</li>
-                <button type='button' onClick={() => handleOpenModal(review.id)}>
-                  삭제
-                </button>
-              </Fragment>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <button type='button' onClick={handleOpenReviewModal}>
-          리뷰 작성하기
-        </button>
-      )}
-      <BestReview snackId={snackId} />
-      -----
-      <ReviewListPage />
+      <div className={styles.reviewWrapper}>
+        {myReview?.length ? (
+          <div />
+        ) : (
+          <>
+            <div className={styles.writeReview}>{snackInfo?.name}의 리뷰를 남겨주세요! </div>
+            <button type='button' onClick={handleOpenReviewModal}>
+              <HiPencilAlt /> 리뷰 작성하기
+            </button>
+          </>
+        )}
+        <div className={styles.reviewTitle}>리뷰</div>
+        <BestReview snackId={snackId} />
+        <ReviewListPage />
+      </div>
     </div>
   )
 }
