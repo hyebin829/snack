59c36600343f6548c916d742a1efa41a274898ac
hyebin829 <smilerain779@gmail.com>
2022-09-20 18:32:22 +0900

    Style : add style

--- front/src/routes/ReviewListPage/index.tsx
+++ front/src/routes/ReviewListPage/index.tsx
@@ -9,6 +9,8 @@ import { IoPersonCircle } from 'react-icons/io5'
 import styles from './reviewlist.module.scss'
 import { openConfirmModal } from 'reducers/modal'
 import LikeButton from 'components/LikeButton'
+import { AiFillStar } from 'react-icons/ai'
+import { RiDeleteBin6Line } from 'react-icons/ri'
 
 const ReviewListPage = () => {
   const params = useParams()
@@ -45,33 +47,48 @@ const ReviewListPage = () => {
 
   return (
     <div>
-      {!reviewList.length && <div>리뷰가 없습니다.</div>}
+      {!reviewList.length && <div className={styles.noReview}>리뷰가 없습니다.</div>}
+
       <ul>
         {reviewList.map((review, index) => (
           <li key={`${review.id}-${review.UserId}-${index + 1}`} className={styles.reviewWrapper}>
-            <ul>
-              <li>{review.UserId}</li>
-              <li>{review.User?.nickname}</li>
-              {review.User?.profileimagesrc ? (
-                <li>
+            <ul className={styles.review}>
+              <li className={styles.userProfile}>
+                {review.User?.profileimagesrc ? (
                   <img
                     src={`http://localhost:3065/profileimage/${review.User?.profileimagesrc}`}
                     alt='profile'
                   />
-                </li>
-              ) : (
-                <IoPersonCircle size={35} />
+                ) : (
+                  <IoPersonCircle size={35} />
+                )}
+                {review.User?.nickname}
+                <span className={styles.rating}>
+                  <AiFillStar size={15} color='#ffa500' />
+                  {review.rating}
+                </span>
+              </li>
+              {myInfo?.id === review.UserId && (
+                <button
+                  type='button'
+                  onClick={() => handleOpenModal(review.id)}
+                  className={styles.deleteButton}
+                >
+                  <span>
+                    <RiDeleteBin6Line size={15} color='#a8a8a8' />
+                  </span>
+                  삭제
+                </button>
               )}
               <li className={styles.createdDate}>{dayjs(review.createdAt).format('YYYY-MM-DD')}</li>
               <li className={styles.reviewcontent}>{review.content}</li>
-              <li className={styles.reviewRating}>{review.rating}</li>
+              <li className={styles.likers}>
+                <span>
+                  <LikeButton review={review} />
+                </span>
+                {review.Likers.length}
+              </li>
             </ul>
-            <LikeButton review={review} /> {review.Likers.length}
-            {myInfo?.id === review.UserId && (
-              <button type='button' onClick={() => handleOpenModal(review.id)}>
-                삭제
-              </button>
-            )}
           </li>
         ))}
       </ul>
