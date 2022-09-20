30407dd260e4134420363dac703c16d058c2fc27
hyebin829 <smilerain779@gmail.com>
2022-09-20 18:38:00 +0900

    Style : add style

--- front/src/components/ConfirmModal/index.tsx
+++ front/src/components/ConfirmModal/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import { removeReview } from 'actions/post'
 import { useAppDispatch, useAppSelector } from 'hooks/useRedux'
 import Portal from 'Portal'
+import { MouseEvent, useRef } from 'react'
 import { closeConfirmModal } from 'reducers/modal'
 import styles from './confirm.module.scss'
 
@@ -8,6 +10,13 @@ const ConfirmModal = () => {
   const { myInfo } = useAppSelector((state) => state.user)
   const { reviewId } = useAppSelector((state) => state.modal)
   const dispatch = useAppDispatch()
+  const modalRef = useRef<HTMLDivElement>(null)
+
+  const clickdiv = (e: MouseEvent<HTMLDivElement>) => {
+    if (modalRef.current === e.target) {
+      dispatch(closeConfirmModal())
+    }
+  }
 
   const deleteReview = () => {
     dispatch(removeReview({ reviewId, userId: myInfo?.id }))
@@ -20,14 +29,19 @@ const ConfirmModal = () => {
 
   return (
     <Portal>
-      <div className={styles.confirmWrapper}>
-        <div>삭제하시겠습니까?</div>
-        <button type='button' onClick={deleteReview}>
-          예
-        </button>
-        <button type='button' onClick={handleCloseModal}>
-          닫기
-        </button>
+      <div className={styles.modalBackground} ref={modalRef} onClick={clickdiv}>
+        <div className={styles.confirmWrapper}>
+          <div className={styles.notice}>알림</div>
+          <div className={styles.content}>리뷰를 삭제하시겠습니까?</div>
+          <div className={styles.buttonWrapper}>
+            <button type='button' onClick={deleteReview}>
+              예
+            </button>
+            <button type='button' onClick={handleCloseModal}>
+              아니오
+            </button>
+          </div>
+        </div>
       </div>
     </Portal>
   )
