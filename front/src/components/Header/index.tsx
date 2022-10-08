d8d7964d57c8045cca8c71e14b7a2a3c012034b4
hyebin829 <smilerain779@gmail.com>
2022-10-08 16:35:29 +0900

    Feat : signup modal , signup button

--- front/src/components/Header/index.tsx
+++ front/src/components/Header/index.tsx
@@ -1,6 +1,8 @@
-import { logout } from 'actions/user'
+import { Link, NavLink } from 'react-router-dom'
+import cx from 'classnames'
 import { useAppDispatch, useAppSelector } from 'hooks/useRedux'
-import { openLoginModal } from 'reducers/modal'
+import { openLoginModal, openSignUpModal } from 'reducers/modal'
+import { logout } from 'actions/user'
 import styles from './header.module.scss'
 
 const Header = () => {
@@ -11,22 +13,52 @@ const Header = () => {
     dispatch(logout())
   }
 
-  const handleOpenModal = () => {
+  const handleOpenLoginModal = () => {
     dispatch(openLoginModal())
+  }
+
+  const handleOpenSignupModal = () => {
+    dispatch(openSignUpModal())
   }
 
   return (
     <header className={styles.header}>
-      <h1>SNACK</h1>
-      {myInfo?.id ? (
-        <button type='button' onClick={handleLogout}>
-          로그아웃
-        </button>
-      ) : (
-        <button type='button' onClick={handleOpenModal}>
-          로그인
-        </button>
-      )}
+      <div className={styles.menu}>
+        <Link to='/'>
+          <h1>SNACKPEDIA</h1>
+        </Link>
+        <ul>
+          <li>
+            <NavLink to='/search' className={({ isActive }) => cx({ [styles.isActive]: isActive })}>
+              과자 검색
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/profile'
+              className={({ isActive }) => cx({ [styles.isActive]: isActive })}
+            >
+              프로필
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.buttonWrapper}>
+        {myInfo?.id ? (
+          <button className={styles.logout} type='button' onClick={handleLogout}>
+            로그아웃
+          </button>
+        ) : (
+          <div>
+            <button type='button' onClick={handleOpenLoginModal}>
+              로그인
+            </button>
+            <button type='button' onClick={handleOpenSignupModal} className={styles.signUpButton}>
+              회원가입
+            </button>
+          </div>
+        )}
+      </div>
     </header>
   )
 }
