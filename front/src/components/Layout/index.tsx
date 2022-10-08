c17af1afaa4a4e70209da56eaacba1a251e3e310
hyebin829 <smilerain779@gmail.com>
2022-10-08 16:34:15 +0900

    Feat : signup modal

--- front/src/components/Layout/index.tsx
+++ front/src/components/Layout/index.tsx
@@ -1,19 +1,19 @@
-import { loadMyInfo } from 'actions/user'
-import GNB from 'components/GNB'
-import Header from 'components/Header'
-import LoginModal from 'components/LoginModal'
-import { useAppDispatch, useAppSelector } from 'hooks/useRedux'
 import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
-
-import styles from './layout.module.scss'
+import { useAppDispatch, useAppSelector } from 'hooks/useRedux'
+import { loadMyInfo } from 'actions/user'
+import Header from 'components/Header'
+import GNB from 'components/GNB'
 import ReviewFormModal from 'components/ReviewFormModal'
 import ConfirmModal from 'components/ConfirmModal'
+import SignUpModal from 'components/SignUpModal'
+import LoginModal from 'components/LoginModal'
+import Footer from 'components/Footer'
+import styles from './layout.module.scss'
 
 const Layout = () => {
-  const { isLoginModalOpen, isConfirmModalOpen, isReviewModalOpen } = useAppSelector(
-    (state) => state.modal
-  )
+  const { isLoginModalOpen, isConfirmModalOpen, isReviewModalOpen, isSignUpModalOpen } =
+    useAppSelector((state) => state.modal)
   const dispatch = useAppDispatch()
 
   useEffect(() => {
@@ -22,26 +22,26 @@ const Layout = () => {
 
   useEffect(() => {
     const $body = document.querySelector('body')
-    if (isReviewModalOpen || isLoginModalOpen || isConfirmModalOpen) {
+    if (isReviewModalOpen || isLoginModalOpen || isConfirmModalOpen || isSignUpModalOpen) {
       $body!.style.overflow = 'hidden'
     }
-
     return () => {
       $body!.style.overflow = 'auto'
     }
-  }, [isConfirmModalOpen, isLoginModalOpen, isReviewModalOpen])
+  }, [isConfirmModalOpen, isLoginModalOpen, isReviewModalOpen, isSignUpModalOpen])
 
   return (
     <div className={styles.layout}>
       <Header />
       <GNB />
-      <main>
+      <main className={styles.main}>
         {isLoginModalOpen && <LoginModal />}
         {isReviewModalOpen && <ReviewFormModal />}
         {isConfirmModalOpen && <ConfirmModal />}
+        {isSignUpModalOpen && <SignUpModal />}
         <Outlet />
       </main>
-      <footer />
+      <Footer />
     </div>
   )
 }
