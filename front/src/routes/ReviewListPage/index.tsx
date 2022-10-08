48daa586703e7a645c05bc6b99bc918d9f059ca6
hyebin829 <smilerain779@gmail.com>
2022-10-08 16:16:01 +0900

    Feat: add loading spiner

--- front/src/routes/ReviewListPage/index.tsx
+++ front/src/routes/ReviewListPage/index.tsx
@@ -1,23 +1,21 @@
-import { loadReviews } from 'actions/post'
-
-import { useAppDispatch, useAppSelector } from 'hooks/useRedux'
 import { useEffect, useRef } from 'react'
-import { useParams } from 'react-router-dom'
 import dayjs from 'dayjs'
-
-import { IoPersonCircle } from 'react-icons/io5'
-import styles from './reviewlist.module.scss'
+import { useParams } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from 'hooks/useRedux'
+import { loadReviews } from 'actions/post'
 import { openConfirmModal } from 'reducers/modal'
 import LikeButton from 'components/LikeButton'
+import Spiner from 'components/Spiner'
+import styles from './reviewlist.module.scss'
 import { AiFillStar } from 'react-icons/ai'
-import { RiDeleteBin6Line } from 'react-icons/ri'
 
 const ReviewListPage = () => {
+  const { reviewList, loadReviewsLoading, hasMoreReview } = useAppSelector((state) => state.post)
+  const { myInfo } = useAppSelector((state) => state.user)
+
   const params = useParams()
   const snackId = params.id
   const dispatch = useAppDispatch()
-  const { reviewList, loadReviewsLoading, hasMoreReview } = useAppSelector((state) => state.post)
-  const { myInfo } = useAppSelector((state) => state.user)
 
   const handleOpenModal = (reviewId: number) => {
     dispatch(openConfirmModal(reviewId))
@@ -47,52 +45,50 @@ const ReviewListPage = () => {
 
   return (
     <div>
-      {!reviewList.length && <div className={styles.noReview}>리뷰가 없습니다.</div>}
-
-      <ul>
-        {reviewList.map((review, index) => (
-          <li key={`${review.id}-${review.UserId}-${index + 1}`} className={styles.reviewWrapper}>
-            <ul className={styles.review}>
-              <li className={styles.userProfile}>
-                {review.User?.profileimagesrc ? (
+      {reviewList.length ? (
+        <ul>
+          {reviewList.map((review, index) => (
+            <li key={`${review.id}-${review.UserId}-${index + 1}`} className={styles.reviewWrapper}>
+              <ul className={styles.review}>
+                <li className={styles.userProfile}>
                   <img
                     src={`http://localhost:3065/profileimage/${review.User?.profileimagesrc}`}
                     alt='profile'
                   />
-                ) : (
-                  <IoPersonCircle size={35} />
-                )}
-                {review.User?.nickname}
-                <span className={styles.rating}>
-                  <AiFillStar size={15} color='#ffa500' />
-                  {review.rating}
-                </span>
-              </li>
-              {myInfo?.id === review.UserId && (
-                <button
-                  type='button'
-                  onClick={() => handleOpenModal(review.id)}
-                  className={styles.deleteButton}
-                >
-                  <span>
-                    <RiDeleteBin6Line size={15} color='#a8a8a8' />
+                  {review.User?.nickname}
+                  <span className={styles.rating}>
+                    <AiFillStar size={15} color='#ffa800' />
+                    {review.rating}
                   </span>
-                  삭제
-                </button>
-              )}
-              <li className={styles.createdDate}>{dayjs(review.createdAt).format('YYYY-MM-DD')}</li>
-              <li className={styles.reviewcontent}>{review.content}</li>
-              <li className={styles.likers}>
-                <span>
-                  <LikeButton review={review} />
-                </span>
-                {review.Likers.length}
-              </li>
-            </ul>
-          </li>
-        ))}
-      </ul>
-      <div ref={target} />
+                </li>
+                {myInfo?.id === review.UserId && (
+                  <button
+                    type='button'
+                    onClick={() => handleOpenModal(review.id)}
+                    className={styles.deleteButton}
+                  >
+                    삭제
+                  </button>
+                )}
+                <li className={styles.createdDate}>
+                  {dayjs(review.createdAt).format('YYYY-MM-DD')}
+                </li>
+                <li className={styles.reviewcontent}>{review.content}</li>
+                <li className={styles.likers}>
+                  <span>
+                    <LikeButton review={review} />
+                  </span>
+                  {review.Likers.length}
+                </li>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        !loadReviewsLoading && <div className={styles.noReview}>리뷰가 없습니다.</div>
+      )}
+      {loadReviewsLoading && <Spiner />}
+      <div ref={target} className={styles.target} />
     </div>
   )
 }
