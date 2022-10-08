e0770026fd11344eef2dc080158b9678d974cce3
hyebin829 <smilerain779@gmail.com>
2022-10-08 16:19:20 +0900

    Style : empty list

--- front/src/routes/MyFavoritePage/index.tsx
+++ front/src/routes/MyFavoritePage/index.tsx
@@ -1,6 +1,6 @@
-import FavoriteButton from 'components/FavoriteButton'
-import { useAppSelector } from 'hooks/useRedux'
 import { Link } from 'react-router-dom'
+import { useAppSelector } from 'hooks/useRedux'
+import FavoriteButton from 'components/FavoriteButton'
 import styles from './myfavorite.module.scss'
 
 const MyFavoritePage = () => {
@@ -9,27 +9,34 @@ const MyFavoritePage = () => {
   return (
     <div className={styles.myfavoriteWrapper}>
       <h2>좋아요 누른 과자</h2>
-      <ul className={styles.favoriteListWrapper}>
-        {myInfo?.Favorited.map((snack) => (
-          <li className={styles.snackWrapper} key={snack.id}>
-            <Link to={`/snack/${snack.id}`}>
-              <ul>
-                <li>
-                  <img
-                    src={`http://localhost:3065/snackimage/${snack.imagesrc}`}
-                    alt={snack.name}
-                  />
-                </li>
-                <li className={styles.snackName}>{snack.name}</li>
-                <li className={styles.snackBrand}>{snack.brand}</li>
-              </ul>
-            </Link>
-            <span className={styles.favoriteButton}>
-              <FavoriteButton snackId={snack.id.toString()} />
-            </span>
-          </li>
-        ))}
-      </ul>
+      {!myInfo?.Favorited.length ? (
+        <div className={styles.emptyList}>
+          좋아하는 과자가 없으신가요? <br />
+          좋아하는 과자를 검색해 목록에 추가해보세요!
+        </div>
+      ) : (
+        <ul className={styles.favoriteListWrapper}>
+          {myInfo?.Favorited.map((snack) => (
+            <li className={styles.snackWrapper} key={snack.id}>
+              <Link to={`/snack/${snack.id}`}>
+                <ul>
+                  <li>
+                    <img
+                      src={`http://localhost:3065/snackimage/${snack.imagesrc}`}
+                      alt={snack.name}
+                    />
+                  </li>
+                  <li className={styles.snackName}>{snack.name}</li>
+                  <li className={styles.snackBrand}>{snack.brand}</li>
+                </ul>
+              </Link>
+              <span className={styles.favoriteButton}>
+                <FavoriteButton snackId={snack.id.toString()} />
+              </span>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
