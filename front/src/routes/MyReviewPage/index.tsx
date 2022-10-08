6cf16251e75c3275421aef60e31591fa1b98f202
hyebin829 <smilerain779@gmail.com>
2022-10-08 16:18:41 +0900

    Refactor : li element

--- front/src/routes/MyReviewPage/index.tsx
+++ front/src/routes/MyReviewPage/index.tsx
@@ -1,16 +1,16 @@
-import { loadMyReviews } from 'actions/post'
-import { useAppDispatch, useAppSelector } from 'hooks/useRedux'
 import { useEffect, useRef } from 'react'
-import styles from './myreview.module.scss'
-
-import LikeButton from 'components/LikeButton'
-import { openConfirmModal } from 'reducers/modal'
-import { RiDeleteBin6Line } from 'react-icons/ri'
 import dayjs from 'dayjs'
+import { useAppDispatch, useAppSelector } from 'hooks/useRedux'
+import { loadMyReviews } from 'actions/post'
+import { openConfirmModal } from 'reducers/modal'
+import LikeButton from 'components/LikeButton'
+import styles from './myreview.module.scss'
+import { AiFillStar } from 'react-icons/ai'
 
 const MyReviewPage = () => {
   const { myInfo } = useAppSelector((state) => state.user)
   const { myReviewList } = useAppSelector((state) => state.post)
+
   const dispatch = useAppDispatch()
   const target = useRef(null)
 
@@ -27,40 +27,44 @@ const MyReviewPage = () => {
   return (
     <div className={styles.reviewPage}>
       <h2>내가 작성한 리뷰</h2>
-      {!myReviewList.length && <div className={styles.noReview}>작성한 리뷰가 없습니다.</div>}
-      <ul>
-        {myReviewList?.map((review, index) => (
-          <li key={`${review.id}-${review.UserId}-${index + 1}`} className={styles.reviewWrapper}>
-            <ul className={styles.review}>
-              <li className={styles.snackImgWrapper}>
-                <img
-                  src={`http://localhost:3065/snackimage/${review.Snack.imagesrc}`}
-                  alt={review.Snack.imagesrc}
-                />
-              </li>
-              <li className={styles.snackNameDate}>
-                {review.Snack.name}
-                <span>{dayjs(review.createdAt).format('YYYY-MM-DD')}</span>
-              </li>
-              <li className={styles.reviewContent}>{review.content}</li>
-              <li className={styles.likers}>
-                <LikeButton review={review} />
-                {review.Likers.length}
-              </li>
-              <button
-                type='button'
-                onClick={() => handleOpenModal(review.id)}
-                className={styles.deleteButton}
-              >
-                <span>
-                  <RiDeleteBin6Line size={15} color='#a8a8a8' />
-                </span>
-                삭제
-              </button>
-            </ul>
-          </li>
-        ))}
-      </ul>
+      {myReviewList.length ? (
+        <ul>
+          {myReviewList?.map((review, index) => (
+            <li key={`${review.id}-${review.UserId}-${index + 1}`} className={styles.reviewWrapper}>
+              <ul className={styles.review}>
+                <li className={styles.snackImgWrapper}>
+                  <img
+                    src={`http://localhost:3065/snackimage/${review.Snack.imagesrc}`}
+                    alt={review.Snack.imagesrc}
+                  />
+                </li>
+                <li className={styles.nameRating}>
+                  <span className={styles.name}>{review.Snack.name}</span>
+                  <span className={styles.rating}>
+                    <AiFillStar size={15} color='#ffa800' />
+                    {review.rating}
+                  </span>
+                </li>
+                <li className={styles.reviewContent}>{review.content}</li>
+                <li className={styles.date}>{dayjs(review.createdAt).format('YYYY-MM-DD')}</li>
+                <li className={styles.likers}>
+                  <span>
+                    <LikeButton review={review} />
+                  </span>
+                  {review.Likers.length}
+                </li>
+                <li className={styles.deleteButton}>
+                  <button type='button' onClick={() => handleOpenModal(review.id)}>
+                    삭제
+                  </button>
+                </li>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className={styles.noReview}>작성한 리뷰가 없습니다.</div>
+      )}
       <div ref={target} />
     </div>
   )
