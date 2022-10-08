31ee9af9e65296dd8907582b9cdc888554b75325
hyebin829 <smilerain779@gmail.com>
2022-10-08 16:16:59 +0900

    Style : need login icon

--- front/src/routes/ProfilePage/index.tsx
+++ front/src/routes/ProfilePage/index.tsx
@@ -1,19 +1,28 @@
-import { useAppSelector } from 'hooks/useRedux'
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { useAppSelector } from 'hooks/useRedux'
 import styles from './profile.module.scss'
 import { IoPersonCircle } from 'react-icons/io5'
 import { AiFillEdit } from 'react-icons/ai'
+import { FaGrinBeamSweat } from 'react-icons/fa'
 
 const ProfilePage = () => {
   const { myInfo } = useAppSelector((state) => state.user)
 
   useEffect(() => {}, [myInfo])
 
-  if (!myInfo) return <div className={styles.needLoginMessage}>로그인이 필요합니다.</div>
+  if (!myInfo)
+    return (
+      <div className={styles.needLoginMessage}>
+        <span>
+          <FaGrinBeamSweat size={100} color='#d7d9dc' />
+        </span>
+        <span>로그인이 필요합니다.</span>
+      </div>
+    )
 
   return (
-    <div>
+    <div className={styles.profilePage}>
       <div className={styles.profileWrapper}>
         {myInfo.profileimagesrc ? (
           <img
@@ -25,15 +34,15 @@ const ProfilePage = () => {
         )}
         <div className={styles.nickname}>{myInfo?.nickname}</div>
         <Link to='/editprofile'>
-          프로필 수정 <AiFillEdit />
+          프로필 수정 <AiFillEdit size={16} />
         </Link>
       </div>
       <div className={styles.menuWrapper}>
         <ul>
-          <li>
+          <li className={styles.myfavorite}>
             <Link to='/myfavorite'>좋아요 누른 과자</Link>
           </li>
-          <li>
+          <li className={styles.myreview}>
             <Link to='/myreview'>내가 작성한 리뷰</Link>
           </li>
         </ul>
